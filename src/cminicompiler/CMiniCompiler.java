2e027291062c5f5fd5c456711cb18800747836e3
Elena Caballero <elenacaballero@Elenas-MacBook-Pro.local>
2017-08-12 16:53:18 -0600

    adding basic comments

--- src/cminicompiler/CMiniCompiler.java
+++ src/cminicompiler/CMiniCompiler.java
@@ -5,6 +5,11 @@
  */
 package cminicompiler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import cminicompiler.LexerGenerator;
+
 /**
  *
  * @author User
@@ -16,6 +21,20 @@ public class CMiniCompiler {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LexerGenerator.main(args);
+        
+        try {
+            Reader reader = new BufferedReader(new FileReader("./test/ejercicio.txt"));
+            CMiniLexer lexer = new CMiniLexer(reader);
+            lexer.next_token();
+
+            System.out.println("No errors.");
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+        
     }
     
 }
